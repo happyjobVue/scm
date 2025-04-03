a3fa4574c8bf2fe997484f2f2a69ccc496b35db7
choiwanmin <choiwanmin@google.com>
2025-04-03 15:04:36 +0900

    Orders OrdersMain, OrdersSearch 페이지 반영

---
+++ src/views/Tasks/Orders.vue
@@ -0,0 +1,8 @@
+<template>
+    <ContextBox>발주 지시서</ContextBox>
+    <OrdersSearch />
+    <OrdersMain />
+</template>
+<script setup>
+import OrdersSearch from '../../components/page/Tasks/Orders/OrdersSearch.vue';
+</script>
