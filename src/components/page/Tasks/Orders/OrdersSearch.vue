2a203b1e6379be463d7f17ede81c7511ced71625
choiwanmin <choiwanmin@google.com>
2025-04-03 15:04:09 +0900

    OrdersSearch 발주지시서 검색 구현

---
+++ src/components/page/Tasks/Orders/OrdersSearch.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import router from '@/router';
+
+const searchTag = ref('productName');
+const searchTitle = ref('');
+const searchStDate = ref('');
+const searchEdDate = ref('');
+
+const options = [
+    { label: '제품명', value: 'productName' },
+    { label: '업체명', value: 'supplyName' },
+];
+
+const handlerSearchOrderList = () => {
+    const query = [];
+    !searchTag || query.push(`searchTag=${searchTag.value}`);
+    !searchTitle || query.push(`searchTitle=${searchTitle.value}`);
+    !searchStDate || query.push(`searchStDate=${searchStDate.value}`);
+    !searchEdDate || query.push(`searchEdDate=${searchEdDate.value}`);
+
+    const queryString = query.length > 0 ? `?${query.join('&')}` : '';
+
+    router.push(queryString);
+};
+
+onMounted(() => {
+    window.location.search && router.replace(window.location.pathname);
+});
+</script>
+<template>
+    <div class="search-box">
+        <select v-model="searchTag">
+            <option
+                v-for="option in options"
+                :key="option.value"
+                :value="option.value"
+            >
+                {{ option.label }}
+            </option>
+        </select>
+        <input v-model.lazy="searchTitle" />
+        <span>
+            <input type="date" v-model="searchStDate" />
+            ~
+            <input type="date" v-model="searchEdDate" />
+        </span>
+        <button @click="handlerSearchOrderList">검색</button>
+    </div>
+</template>
+<style lang="scss" scoped>
+.search-box {
+    margin-bottom: 10px;
+    display: block;
+    float: inline-end;
+}
+
+input {
+    padding: 8px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-right: 5px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+}
+
+select {
+    padding: 8px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+    margin-right: 5px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+}
+
+button {
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border: none;
+    color: white;
+    width: 70px;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    font-size: 12px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 12px;
+    box-shadow: 0 4px #999;
+    background-color: #3bb2ea;
+
+    &:hover {
+        background-color: #45a049;
+    }
+
+    &:active {
+        background-color: #3e8e41;
+        box-shadow: 0 2px #666;
+        transform: translateY(2px);
+    }
+}
+</style>
